9746963f57385dce5d00a3efaff4d9a38dc3e994
Chihalu <m.chihiro0314@gmail.com>
2026-09-24 09:03:58 +0900

    first commit

---
+++ src/client/java/chihalu/mina/horror/client/MinaHorrorDataGenerator.java
@@ -0,0 +1,11 @@
+package chihalu.mina.horror.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MinaHorrorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
